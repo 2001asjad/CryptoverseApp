fef6bd9ac4390f47c8289bd21a115d721e416972
Asjad Ahmed Khan <asjadahmedkhan@Asjads-MacBook-Air.local>
2022-04-23 03:10:45 +0530

    Made the final changes

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
     'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '2f0b3757e4msh560f85af4f88001p1839d2jsn7d3dc270f929'
+    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 };
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+const baseUrl = process.env.REACT_APP_NEWS_API_URL;
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
